1013ea15bc26363437bd0ac707f1a5b04b39046f
Afy Wahyu <afyww18@gmail.com>
2024-05-01 16:55:22 +0700

    upp

--- src/components/Contactus.jsx
+++ src/components/Contactus.jsx
@@ -3,14 +3,14 @@
 function Contactus() {
     return (
         <div className='grid grid-cols-1  bg-black'>
-            <div className="my-32 xl:my-36 2xl:my-52 mx-5 xl:mx-48 2xl:mx-52">
+            <div className="my-36 xl:my-36 2xl:my-52 mx-5 xl:mx-48 2xl:mx-52">
                 <div className="space-y-8 xl:space-y-24 2xl:space-y-36">
                     <div className="space-y-2 xl:space-y-4">
                     <h1 className="text-2xl text-center xl:text-center xl:text-6xl 2xl:text-7xl text-white  font-medium">Contact Us</h1>
                     </div>
                     <form className="space-y-2 xl:space-y-8 2xl:space-y-16">
                         <div className="grid grid-cols-1 xl:grid-cols-2 space-y-5 xl:space-y-0">
-                            <div className="mx-10">
+                            <div className="mx-5 xl:mx-10">
                                 <div className="">
                                     <label className="text-sm xl:text-lg 2xl:text-xl text-extralight text-white text-left">*Name</label>
                                 </div>
@@ -24,7 +24,7 @@ function Contactus() {
                                     />
                                 </div>
                             </div>
-                            <div className="">
+                            <div className="mx-5 xl:mx-10">
                                 <div className="">
                                     <label className="text-sm xl:text-lg 2xl:text-xl text-extralight text-white text-left">*Email</label>
                                 </div>
@@ -40,7 +40,7 @@ function Contactus() {
                             </div>
                         </div>
                         <div className="">
-                            <div className="mx-10">
+                            <div className="mx-5 xl:mx-10">
                                 <div className="">
                                     <label className="text-sm xl:text-lg 2xl:text-xl text-extralight text-white text-left">*Pesan</label>
                                 </div>
