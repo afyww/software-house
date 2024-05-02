4d22317c0ba75e006b3c2aed9a860581f917260d
Afy Wahyu <afyww18@gmail.com>
2024-05-02 12:28:37 +0700

    upp

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -2,14 +2,51 @@
 function Aboutus() {
     return (
         <div className='grid grid-cols-1 bg-gradient-to-br from-black to-amber-800'>
-            <div className="my-64 xl:my-64 2xl:my-80">
-                <div className="xl:order-1 mx-5 xl:mx-32 order-2 space-y-5 xl:space-y-10 2xl:space-y-16">
-                    <div className="w-full">
-                        <h1 className="text-2xl text-center xl:text-left xl:text-6xl 2xl:text-7xl text-white  font-medium">About Us</h1>
-                        <h1 className="text-2xl text-center xl:text-left xl:text-6xl 2xl:text-7xl text-amber-500  font-medium">Your Brand And</h1>
+            <div className="my-44 xl:my-44 2xl:my-56">
+                <div className="mx-5 xl:mx-32 order-2 space-y-5 xl:space-y-10 2xl:space-y-16">
+                    <div className="flex justify-between">
+                        <div>
+                            <h1 className="text-3xl text-center xl:text-left xl:text-6xl 2xl:text-7xl text-white font-semibold">About Us</h1>
+                        </div>
+                        <div className="w-1/2">
+                            <p className="line-clamp-2 text-sm xl:text-base 2xl:text-lg text-center xl:text-right font-light text-amber-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
                     </div>
-                    <div className="xl:w-full">
-                        <p className="text-sm xl:text-base 2xl:text-lg text-center xl:text-left font-light text-white">Dipercaya para klien selama 9 tahun berkarya, kami siap membantu untuk membangun dan mengembangkan strategi bisnis anda</p>
+                    <div className="p-4">
+                        <div className="grid grid-cols-1 xl:grid-cols-4 gap-4">
+                        <div className="bg-white bg-opacity-20 space-y-5 p-8 xl:p-12 rounded-3xl">
+                            <div className="text-center">
+                                <h1 className="text-5xl xl:text-6xl font-bold text-white">18+</h1>
+                            </div>
+                            <div className="mx-auto text-center">
+                                <p className="text-white text-base">Lorem ipsum dolor sit amet, consectetur</p>
+                            </div>
+                        </div>
+                        <div className="bg-white bg-opacity-20 space-y-5 p-8 xl:p-12 rounded-3xl">
+                            <div className="text-center">
+                                <h1 className="text-5xl xl:text-6xl font-bold text-white">18+</h1>
+                            </div>
+                            <div className="mx-auto text-center">
+                                <p className="text-white text-base">Lorem ipsum dolor sit amet, consectetur</p>
+                            </div>
+                        </div>
+                        <div className="bg-white bg-opacity-20 space-y-5 p-8 xl:p-12 rounded-3xl">
+                            <div className="text-center">
+                                <h1 className="text-5xl xl:text-6xl font-bold text-white">18+</h1>
+                            </div>
+                            <div className="mx-auto text-center">
+                                <p className="text-white text-base">Lorem ipsum dolor sit amet, consectetur</p>
+                            </div>
+                        </div>
+                        <div className="bg-white bg-opacity-20 space-y-5 p-8 xl:p-12 rounded-3xl">
+                            <div className="text-center">
+                                <h1 className="text-5xl xl:text-6xl font-bold text-white">18+</h1>
+                            </div>
+                            <div className="mx-auto text-center">
+                                <p className="text-white text-base">Lorem ipsum dolor sit amet, consectetur</p>
+                            </div>
+                        </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/Serviceus.jsx
+++ src/components/Serviceus.jsx
@@ -3,7 +3,7 @@
 function Serviceus() {
     return (
         <div className='grid grid-cols-1 bg-gradient-to-tr from-black to-amber-800'>
-            <div className="my-64 xl:my-64 2xl:my-80">
+            <div className="my-64 xl:my-32 2xl:my-80">
                 <div className="xl:order-1 mx-5 xl:mx-32 order-2 space-y-5 xl:space-y-10 2xl:space-y-16">
                     <div className="w-full">
                         <h1 className="text-2xl text-center xl:text-left xl:text-6xl 2xl:text-7xl text-white  font-medium">Service Us</h1>
